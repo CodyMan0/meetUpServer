3cfa3863b39f8adc5f3cbcf4f2db2e23522477e0
Brian LEE <93697790+CodyMan0@users.noreply.github.com>
2023-02-22 13:04:31 +0900

    feat: 서버 세팅 완료

--- index.js
+++ index.js
@@ -2,25 +2,52 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from dotenv
+import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import path from "path"
+import path from "path";
 import { fileURLToPath } from "url";
 
 //CONFIGURATION
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
-const app = express()
+const app = express();
 app.use(express.json());
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json({limit: "30mb", extends: true} ));
-app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extends: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use('/asset' , express.static(path.join(__dirname, 'public/assets')))
+// 간단하게 로컬에서 이미지 저장하도록 설정
+app.use("/asset", express.static(path.join(__dirname, "public/assets")));
 
+//file storage
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "/public/asset");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
 
+const upload = multer({ storage });
+
+//MONGOOSE SETUP
+
+const PORT = process.env.PORT || 6001;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+	})
+	.catch((error) => console.log(`${error} didn't connected `));
